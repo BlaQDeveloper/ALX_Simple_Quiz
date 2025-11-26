e77211b3fa3f4764442c6a931aa0a5b428ed13e7
BlaQDeveloper <ikechukwudavid331@gmail.com>
2025-11-26 09:09:37 +0100

    Hope this works!

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer() {
+    let correctAnswer = "4";
+    const userAnswer = document.querySelector("input[name='quiz']").value();
+
+    if (userAnswer === correctAnswer) {
+        feedback.textContext = "Correct! Well done";
+    }
+    else {
+        feedback.textContext = "That's incorrect. Try again";
+    }
+
+    const submitButton = document.getElementById("submit-answer");
+    submitButton.addEventListener("click", checkAnswer);
+
+}
